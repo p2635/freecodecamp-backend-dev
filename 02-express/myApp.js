72d5e1f5102fb0cdd92f71e81d2233f03361036a
p2635 <4522927+p2635@users.noreply.github.com>
2024-01-30 13:59:56 +0000

    Serve css asset with middleware

--- 02-express/myApp.js
+++ 02-express/myApp.js
@@ -5,6 +5,8 @@ console.log("Hello World");
 
 const pathToIndex = __dirname + "/views/index.html";
 
+app.use("/public", express.static(__dirname + "/public"));
+
 app.get("/", function (req, res) {
   res.sendFile(pathToIndex);
 });
