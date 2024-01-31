31f016d47693800629be87473ca583de91b781d2
p2635 <4522927+p2635@users.noreply.github.com>
2024-01-31 20:46:05 +0000

    Implement answers to first two tests

--- 04-project-timestamp-microservice/index.js
+++ 04-project-timestamp-microservice/index.js
@@ -24,6 +24,15 @@ app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
 });
 
+app.get("/api/:date", function (req, res) {
+  let date = new Date(req.params.date);
+  if (isNaN(date)) {
+    res.json({ error: "Invalid Date" });
+  } else {
+    res.json({ unix: Date.parse(date), utc: date.toUTCString() });
+  }
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
