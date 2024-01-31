1ebbc99f8c09285157636a4d5dab53bb9be7ff27
p2635 <4522927+p2635@users.noreply.github.com>
2024-01-31 19:55:58 +0000

    Implement queryChain

--- 03-mongoose/myApp.js
+++ 03-mongoose/myApp.js
@@ -117,8 +117,14 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person.find({ favoriteFoods: foodToSearch })
+    .sort("name")
+    .limit(2)
+    .select("-age")
+    .exec((err, query) => {
+      if (err) console.error(err);
+      done(null, query);
+    });
 };
 
 /** **Well Done !!**
