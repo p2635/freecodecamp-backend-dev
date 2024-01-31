6308b7051cb6230efbc04928b15a12d38813cc57
p2635 <4522927+p2635@users.noreply.github.com>
2024-01-31 19:41:46 +0000

    Implement removeManyPeople

--- 03-mongoose/myApp.js
+++ 03-mongoose/myApp.js
@@ -109,8 +109,10 @@ const removeById = (personId, done) => {
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-
-  done(null /*, data*/);
+  Person.remove({ name: nameToRemove }, (err, outcome) => {
+    if (err) console.error(err);
+    done(null, outcome);
+  });
 };
 
 const queryChain = (done) => {
