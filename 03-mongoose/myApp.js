261453d1e42eb6d8cfbd114eded5e8a2d914510e
p2635 <4522927+p2635@users.noreply.github.com>
2024-01-31 19:33:25 +0000

    Refactor findAndUpdate

--- 03-mongoose/myApp.js
+++ 03-mongoose/myApp.js
@@ -89,8 +89,15 @@ const findEditThenSave = (personId, done) => {
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-
-  done(null /*, data*/);
+  Person.findOneAndUpdate(
+    { name: personName },
+    { age: ageToSet },
+    { new: true },
+    (err, updatedPerson) => {
+      if (err) return console.error(err);
+      done(null, updatedPerson);
+    }
+  );
 };
 
 const removeById = (personId, done) => {
